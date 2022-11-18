ad264609d3af0880922efc5aeb6255f5d38455a4
Amy <wilford.amy@gmail.com>
2022-11-18 16:27:12 -0500

    laidout typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,6 +3,45 @@ const {gql} = require('apollo-server-express');
 // Define typeDefs which include Query and Mutation types
 const typeDefs = gql`
 
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int
+    savedBooks: [Book]
+}
+type Book {
+    bookId: ID!
+    authors:  [String]
+    description: String!
+    title: String
+    image: String
+    link: String
+}
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+input NewBook {
+    authors: [String]
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(usernmae: String!, email: String!, password: String!): Auth
+    saveBook(newBook: NewBook!): User
+    removeBook(bookId: ID!): User
+}
 `;
 
 // export typeDefs for use in application
