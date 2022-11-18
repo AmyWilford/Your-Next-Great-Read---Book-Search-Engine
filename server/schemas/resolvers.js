9bd236575632db6837624a2632836565d06fd565
Amy <wilford.amy@gmail.com>
2022-11-18 17:04:26 -0500

    created resolvers

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,63 @@
+const {AuthenticationError} = require('apollo-server-express');
+const {User, Book} = require('../models');
+const {signToken} = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async(parent,args, context)=> {
+            if(context.user){
+                return User.findOne({_id: context.user._id});
+            }
+            throw new AuthenticationError('It looks like you are not logged in')
+        },
+    },
+
+    Mutation: {
+        login: async(parent, {email, password})=> {
+            const user = await User.findOne({email});
+
+            if(!user) {
+                throw new AuthenticationError('could not find an account with this username')
+            }
+            const correctPw = await user.isCorrectPassword (password);
+
+            if(!correctPw){
+                throw new AuthenticationError('incorrect password');
+            }
+
+            const token = signToken(user);
+            return {token, user};
+        },
+        addUser: async(parent, {name, email, password}) => {
+            const user = await User.create({name, email, password});
+            const token = signToken(user);
+            return {token, user};
+        },
+
+        saveBook: async(parent, { newBook}, context)=> {
+            if(context.user){
+                const updatedUser = await Book.findOneAndUpdate(
+                    { _id:context.user._id},
+                    {$addToSet: {savedBooks: newBook}}, 
+                    {new: true}
+                    );
+                    return updatedUser
+            };
+            throw new AuthenticationError('It looks like you are not logged in!') 
+        },
+
+        removeBook: async(parent, {bookId}, context)=> {
+            if(context.user) {
+                const updatedUser = await Book.findByIdAndUpdate(
+                    {_id:context.user._id}, 
+                    {$pull: {savedbook: {bookId}}}, //is this right? 
+                    {new: true}
+                )
+                return updatedUser;
+            };
+            throw new AuthenticationError('It looks like you are not logged in!')
+        }
+    }
+}
+
+module.exports = resolvers;
